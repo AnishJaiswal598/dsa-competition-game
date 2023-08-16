750323e9509d4179191a9de8c75929a82ae62b48
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2023-08-16 19:24:05 +0530

    change env path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({path:'../.env'});
 import passport from 'passport';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
